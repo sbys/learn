abff9dd34582d50d508b62a20ce66e80a3781313
unknown <shuai.yuan@hirain.com>
2019-12-24 11:17:48 +0800

    最后提交

--- eureka-provider/src/main/java/com/example/eurekaprovider/controller/RefactorHelloController.java
+++
@@ -1,6 +0,0 @@
-package com.example.eurekaprovider.controller;
-
-public class RefactorHelloController
-//        implements HelloService
-{
-}
---
+++ ssmdemo/src/main/java/com/example/ssmdemo/config/MyBatisConfig.java
@@ -0,0 +1,4 @@
+package com.example.ssmdemo.config;
+
+public class MyBatisConfig {
+}
